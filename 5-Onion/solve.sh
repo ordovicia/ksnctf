bb8f5b50a6f220ca15672c38aa4a4e24295f38ea
Hidehito Yabuuchi <hdht.ybuc@gmail.com>
2017-08-23 17:19:49 +0900

    Add 4-VillagerA

--- 5-Onion/solve.sh
+++ 5-Onion/solve.sh
@@ -1,7 +1,7 @@
 code="$(cat code.txt)"
 
 for i in $(seq 16); do
-    code="$(echo "$code" | base64 -d)"
+    code="$(echo "$code" | base64 --decode)"
 done
 
 echo "$code" | uudecode -o /dev/stdout
