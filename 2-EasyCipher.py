52205abe5b6b32213dfe7f04f08245d1253b9b25
Hidehito Yabuuchi <hdht.ybuc@gmail.com>
2017-08-24 01:05:10 +0900

    Polish 3-CrawlingChaos

--- 2-EasyCipher.py
+++ 2-EasyCipher.py
@@ -1,10 +1,10 @@
 code = "EBG KVVV vf n fvzcyr yrggre fhofgvghgvba pvcure gung ercynprf n yrggre jvgu gur yrggre KVVV yrggref nsgre vg va gur nycunorg. EBG KVVV vf na rknzcyr bs gur Pnrfne pvcure, qrirybcrq va napvrag Ebzr. Synt vf SYNTFjmtkOWFNZdjkkNH. Vafreg na haqrefpber vzzrqvngryl nsgre SYNT."
 
-alph_num = 26
+ALPHABET_NUM = 26
 A = 65
-Z = A + alph_num
+Z = A + ALPHABET_NUM
 a = 97
-z = a + alph_num
+z = a + ALPHABET_NUM
 
 rot = -13
 print("rot =", rot)
@@ -12,9 +12,9 @@ print("rot =", rot)
 for c in code:
     o = ord(c)
     if A <= o <= Z:
-        o = (o - A + rot) % alph_num + A
+        o = (o - A + rot) % ALPHABET_NUM + A
     elif a <= o <= z:
-        o = (o - a + rot) % alph_num + a
+        o = (o - a + rot) % ALPHABET_NUM + a
     c = chr(o)
     print(c, end="")
 print()
--- 3-CrawlingChaos/invert.py
+++ 3-CrawlingChaos/invert.py
@@ -1,13 +1,4 @@
 p = [70, 152, 195, 284, 475, 612, 791, 896, 810, 850, 737, 1332, 1469, 1120, 1470, 832, 1785, 2196, 1520, 1480, 1449];
-flag = []
-
-i = 0
-for q in p:
-    if i != 0:
-        flag.append(chr(q // (i + 1)))
-    else:
-        flag.append(chr(q))
-    i = i + 1
-
+flag = [chr(q // (i + 1)) for i, q in enumerate(p)]
 flag = ''.join(flag)
 print(flag)
